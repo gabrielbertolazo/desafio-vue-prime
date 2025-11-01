dfc7fdb4b446bdfccdbc422044449b082d29c604
Gabriel Bertolazo <gabriel.bertolazo@rentcars.com>
2025-11-01 12:04:52 +0100

    feature: configura dependencias primevue e tailwind

--- src/pages/Home/HomePage.vue
+++ src/pages/Home/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="home-page">
-        <h1>Welcome to the Home Page</h1>
+        <h1 class="text-3xl font-bold underline">Welcome to the Home Page</h1>
         <p>This is the main landing page of the application.</p>
     </main>
 </template>
